de56b8fc3cd1ca55f5ae30566f9451aa0227992c
JonnyCol <31376273+JonnyCol@users.noreply.github.com>
2022-07-02 20:46:49 -0400

    Delete frontend directory

--- frontend/app/common/order-contents.spec.ts
+++
@@ -1,7 +0,0 @@
-import { OrderContents } from './order-contents';
-
-describe('OrderContents', () => {
-  it('should create an instance', () => {
-    expect(new OrderContents()).toBeTruthy();
-  });
-});
